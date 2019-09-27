169385bbcb160033b4eb3261ec6b0406a8d1c309
papainformatico <info@papainformatico.es>
2019-09-28 01:00:12 +0200

    Agregar archivos de proyecto.

---
+++ OstPathHelper/mainForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace OstPathHelper
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.source_file_path_label = new System.Windows.Forms.Label();
+            this.target_file_path_label = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status_label = new System.Windows.Forms.ToolStripStatusLabel();
+            this.original_file_path_txt = new System.Windows.Forms.TextBox();
+            this.target_file_path_txt = new System.Windows.Forms.TextBox();
+            this.source_file_path_btn = new System.Windows.Forms.Button();
+            this.target_file_path_btn = new System.Windows.Forms.Button();
+            this.go_btn = new System.Windows.Forms.Button();
+            this.close_outlook_process_btn = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // source_file_path_label
+            // 
+            this.source_file_path_label.AutoSize = true;
+            this.source_file_path_label.Location = new System.Drawing.Point(10, 18);
+            this.source_file_path_label.Name = "source_file_path_label";
+            this.source_file_path_label.Size = new System.Drawing.Size(90, 13);
+            this.source_file_path_label.TabIndex = 0;
+            this.source_file_path_label.Text = "Source_file_path:";
+            // 
+            // target_file_path_label
+            // 
+            this.target_file_path_label.AutoSize = true;
+            this.target_file_path_label.Location = new System.Drawing.Point(10, 50);
+            this.target_file_path_label.Name = "target_file_path_label";
+            this.target_file_path_label.Size = new System.Drawing.Size(84, 13);
+            this.target_file_path_label.TabIndex = 1;
+            this.target_file_path_label.Text = "Target file path: ";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status_label});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 112);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(615, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status_label
+            // 
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(152, 17);
+            this.status_label.Text = "Outlook process not found.";
+            // 
+            // original_file_path_txt
+            // 
+            this.original_file_path_txt.Location = new System.Drawing.Point(100, 15);
+            this.original_file_path_txt.Name = "original_file_path_txt";
+            this.original_file_path_txt.Size = new System.Drawing.Size(411, 20);
+            this.original_file_path_txt.TabIndex = 3;
+            // 
+            // target_file_path_txt
+            // 
+            this.target_file_path_txt.Location = new System.Drawing.Point(100, 46);
+            this.target_file_path_txt.Name = "target_file_path_txt";
+            this.target_file_path_txt.Size = new System.Drawing.Size(411, 20);
+            this.target_file_path_txt.TabIndex = 4;
+            // 
+            // source_file_path_btn
+            // 
+            this.source_file_path_btn.Location = new System.Drawing.Point(517, 13);
+            this.source_file_path_btn.Name = "source_file_path_btn";
+            this.source_file_path_btn.Size = new System.Drawing.Size(33, 23);
+            this.source_file_path_btn.TabIndex = 5;
+            this.source_file_path_btn.Text = "...";
+            this.source_file_path_btn.UseVisualStyleBackColor = true;
+            this.source_file_path_btn.Click += new System.EventHandler(this.Original_file_path_btn_Click);
+            // 
+            // target_file_path_btn
+            // 
+            this.target_file_path_btn.Location = new System.Drawing.Point(517, 44);
+            this.target_file_path_btn.Name = "target_file_path_btn";
+            this.target_file_path_btn.Size = new System.Drawing.Size(33, 23);
+            this.target_file_path_btn.TabIndex = 6;
+            this.target_file_path_btn.Text = "...";
+            this.target_file_path_btn.UseVisualStyleBackColor = true;
+            this.target_file_path_btn.Click += new System.EventHandler(this.Target_file_path_btn_Click);
+            // 
+            // go_btn
+            // 
+            this.go_btn.Location = new System.Drawing.Point(517, 73);
+            this.go_btn.Name = "go_btn";
+            this.go_btn.Size = new System.Drawing.Size(75, 23);
+            this.go_btn.TabIndex = 7;
+            this.go_btn.Text = "Go!";
+            this.go_btn.UseVisualStyleBackColor = true;
+            this.go_btn.Click += new System.EventHandler(this.Go_btn_Click);
+            // 
+            // close_outlook_process_btn
+            // 
+            this.close_outlook_process_btn.Location = new System.Drawing.Point(12, 73);
+            this.close_outlook_process_btn.Name = "close_outlook_process_btn";
+            this.close_outlook_process_btn.Size = new System.Drawing.Size(158, 23);
+            this.close_outlook_process_btn.TabIndex = 8;
+            this.close_outlook_process_btn.Text = "Close Outlook process";
+            this.close_outlook_process_btn.UseVisualStyleBackColor = true;
+            this.close_outlook_process_btn.Click += new System.EventHandler(this.Close_outlook_process_btn_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 134);
+            this.Controls.Add(this.close_outlook_process_btn);
+            this.Controls.Add(this.go_btn);
+            this.Controls.Add(this.target_file_path_btn);
+            this.Controls.Add(this.source_file_path_btn);
+            this.Controls.Add(this.target_file_path_txt);
+            this.Controls.Add(this.original_file_path_txt);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.target_file_path_label);
+            this.Controls.Add(this.source_file_path_label);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OstPathHelper";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label source_file_path_label;
+        private System.Windows.Forms.Label target_file_path_label;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status_label;
+        private System.Windows.Forms.TextBox original_file_path_txt;
+        private System.Windows.Forms.TextBox target_file_path_txt;
+        private System.Windows.Forms.Button source_file_path_btn;
+        private System.Windows.Forms.Button target_file_path_btn;
+        private System.Windows.Forms.Button go_btn;
+        private System.Windows.Forms.Button close_outlook_process_btn;
+    }
+}
+
---
+++ OstPathHelper/mainForm.cs
@@ -0,0 +1,187 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace OstPathHelper
+{
+    public partial class mainForm : Form
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private  String source_file_path;
+        private String target_file_path;
+        private String complete_target_file_path;
+
+
+        [DllImport("kernel32.dll")]
+        static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+        static int SYMLINK_FLAG_DIRECTORY = 1;
+        static int SYMLINK_FLAG_FILE = 0;
+        public mainForm()
+        {
+            InitializeComponent();
+
+            if (checkProcessRunning("outlook")){
+                status_label.Text = "Found Outlook Running Process!";
+                status_label.ForeColor = Color.Red;
+                close_outlook_process_btn.Enabled = true;
+                go_btn.Enabled = false;
+            }
+            else
+            {
+                status_label.Text = "Outlook process not found.";
+                status_label.ForeColor = Color.Green;
+                close_outlook_process_btn.Enabled = false;
+                go_btn.Enabled = true;
+
+            }
+        }
+
+
+        private Boolean checkProcessRunning(string process_name)
+        {
+            Process[] pname = Process.GetProcessesByName(process_name);
+            if (pname.Length == 0)
+                return false;
+            else
+                return true;
+        }
+        private void Close_outlook_process_btn_Click(object sender, EventArgs e)
+        {
+            if (killProcess("outlook"))
+            {
+                status_label.Text = "Outlook process not found.";
+                status_label.ForeColor = Color.Green;
+                close_outlook_process_btn.Enabled = false;
+                go_btn.Enabled = true;
+
+            }
+            else
+            {
+                MessageBox.Show("Outlook process could not be kille, please close Outlook manually.");
+            }
+        }
+        private Boolean killProcess(string process_name)
+        {
+            try
+{
+                foreach (Process proc in Process.GetProcessesByName(process_name))
+                {
+                    proc.Kill();
+                  
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(process_name + " process could not be killed");
+                logger.Error(ex.StackTrace);
+                return true;
+            }
+        }
+
+        private String select_directory()
+        {
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                return dialog.FileName;
+            }
+            return null;
+        }
+
+        private String select_file()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    return openFileDialog.FileName;
+
+                }
+            }
+            return null;
+        }
+
+        private void Original_file_path_btn_Click(object sender, EventArgs e)
+        {
+           
+            
+
+            source_file_path = select_file();
+            if (!string.IsNullOrEmpty(source_file_path))
+            {
+                original_file_path_txt.Text = source_file_path;
+            }
+        }
+
+        private void Target_file_path_btn_Click(object sender, EventArgs e)
+        {
+            target_file_path = select_directory();
+            if (!string.IsNullOrEmpty(target_file_path))
+            {
+                target_file_path_txt.Text = target_file_path;
+            }
+        }
+
+        private void Go_btn_Click(object sender, EventArgs e)
+        {
+
+            if ((!string.IsNullOrEmpty(source_file_path)) && (!string.IsNullOrEmpty(target_file_path))){
+                if (moveFile())
+                {
+                    if (CreateSymbolicLink(source_file_path, complete_target_file_path, SYMLINK_FLAG_FILE))
+                    {
+                        MessageBox.Show("Process done !.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed creating link: " + source_file_path + " <===> " + complete_target_file_path);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed moving File: " + source_file_path + " to " + complete_target_file_path);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must select the source file and destination folder");
+            }
+
+        }
+
+        private Boolean moveFile()
+        {
+            try
+            {
+                complete_target_file_path = Path.Combine(target_file_path, Path.GetFileName(source_file_path));
+                File.Move(source_file_path, complete_target_file_path);
+                logger.Info("Moved file " + source_file_path + " to " + complete_target_file_path);
+                return true;
+            }
+            catch (IOException ex)
+            {
+                logger.Info("Failed to move file " + source_file_path + " to " + complete_target_file_path);
+                return false;
+            }
+        }
+    }
+}
