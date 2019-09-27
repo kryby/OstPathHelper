aee150e7c989a8d7bd54e838cdbe33787d26792e
papainformatico <info@papainformatico.es>
2019-09-28 01:29:02 +0200

    first beta version

--- OstPathHelper/mainForm.cs
+++ OstPathHelper/mainForm.cs
@@ -1,13 +1,7 @@
 ﻿using NLog;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.IO;
@@ -25,7 +19,7 @@ namespace OstPathHelper
 
         [DllImport("kernel32.dll")]
         static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
-        static int SYMLINK_FLAG_DIRECTORY = 1;
+        
         static int SYMLINK_FLAG_FILE = 0;
         public mainForm()
         {
@@ -180,6 +174,7 @@ namespace OstPathHelper
             catch (IOException ex)
             {
                 logger.Info("Failed to move file " + source_file_path + " to " + complete_target_file_path);
+                logger.Error(ex.StackTrace);
                 return false;
             }
         }
